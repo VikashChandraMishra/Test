0368812ed67187ec62373230b1c42c891b8c0cb5
VikashChandraMishra <crimsonsyrus000@gmail.com>
2022-11-29 18:25:50 +0530

    Working on admin interface

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Applicant = require('../models/Applicant');
 
 router.post('/login', (req, res) => {
     const { ADMIN_USERNAME, ADMIN_PASSWORD } = process.env;
     if (req.body.username === ADMIN_USERNAME && req.body.password === ADMIN_PASSWORD) {
-        res.cookie('user', 'admin')
-        res.json({"success": true, "message": "admin verified"})
+        res.cookie('username', 'admin')
+        res.json({ "success": true, "message": "admin verified" })
     }
     else
-        res.json({"success": false, "message": "admin not verified"})
+        res.json({ "success": false, "message": "admin not verified" })
+})
+
+router.get('/fetch-applicant-list', async (req, res) => {
+    try {
+        const applicants = await Applicant.find();
+        res.json({"success": true, applicants});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error!");
+    }
 })
 
 
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,9 +1,66 @@
+import { useEffect, useMemo, useState } from "react";
 import Sidebar from "./Sidebar";
+import Table from "./Table";
 
 const Dashboard = () => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://127.0.0.1:5000/api/admin/fetch-applicant-list', {
+        method: 'GET',
+
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      const json = await response.json();
+      if (json.success) {
+        setData(json.applicants);
+      }
+      else alert("Cannot fetch applicant list at the moment!");
+    }
+    fetchData();
+  })
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "List",
+        columns: [
+          {
+            Header: "FirstName",
+            accessor: "firstname"
+          },
+          {
+            Header: "MiddleName",
+            accessor: "middlename"
+          },
+          {
+            Header: "LastName",
+            accessor: "lastname"
+          },{
+            Header: "DOB",
+            accessor: "dob"
+          },{
+            Header: "Category",
+            accessor: "category"
+          },{
+            Header: "Qualification",
+            accessor: "qualification"
+          },
+        ]
+      },
+    ],
+    []
+  );
+
   return (
-    <div className="">
-        <Sidebar />
+    <div>
+      {/* <Sidebar /> */}
+      <Table columns={columns} data={data} />
     </div>
   )
 }
